c1e9082f64d734261cfce4891130c9cce022e555
Varninder Rai <varninder@openmailbox.org>
2017-06-28 15:54:57 -0700

    create function to search by ingredients

--- RecipeFinder/RecipeFinder-Bridging-Header.h
+++ RecipeFinder/RecipeFinder-Bridging-Header.h
@@ -2,3 +2,15 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#ifndef Header_h
+#define Header_h
+
+
+#import <UIkit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+#import "UserInputViewController.h"
+#import "Recipe.h"
+
+#endif
+
