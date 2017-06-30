2694d71e18290e5dc79552871b091a6d1d4c8d81
Varninder Rai <varninder@openmailbox.org>
2017-06-29 17:23:18 -0700

    added array for recipes

--- RecipeFinder/RecipesTableView.m
+++ RecipeFinder/RecipesTableView.m
@@ -8,10 +8,12 @@
 
 #import "RecipesTableView.h"
 #import "RecipeTVC.h"
+#import "DRPLoadingSpinner.h"
 
 @interface RecipesTableView ()
 
 @property (nonatomic) NSMutableArray *recipes;
+@property DRPLoadingSpinner *spinner;
 
 @end
 
@@ -32,7 +34,6 @@
     self.title = @"My Recipes";
 
     [self prepareUserRecipes];
-    
 }
 
 #pragma mark - prepare Array of Data
@@ -41,6 +42,7 @@
 - (void) prepareUserRecipes{
     
     //here.. for creating Session for json and populate info out of url of API and add that object to the array: recipes
+    
     
     NSString *formattedIngredients = [self.recipe.userIngredients stringByReplacingOccurrencesOfString:@"," withString:@"%2C"];
     NSString *ingredientsURL = [NSString stringWithFormat:@"https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=%@&limitLicense=false&number=30&ranking=1", formattedIngredients];
@@ -86,15 +88,16 @@
                     } else {
                         for(NSDictionary *dictionary in recipeDetails) {
                             NSArray *steps = [dictionary objectForKey:@"steps"];
+                            NSMutableArray *recipeSteps = [[NSMutableArray alloc]init];
                             for(NSDictionary *step in steps) {
-                                NSString *stringRecipeDetails = [step objectForKey:@"step"];
-                                
+                               // NSString *stringRecipeDetails = [step objectForKey:@"step"];
                                 //creating a recipe object and storing it in the array
-                                self.recipe = [[Recipe alloc] initWithRecipeImage:aPIRecipeImage andRecipeID:stringRecipeID andRecipeTitle:stringRecipeTitle andRecipeDetails:stringRecipeDetails];
+                                [recipeSteps addObject:[step objectForKey:@"step"]];
+                            }
+                                self.recipe = [[Recipe alloc] initWithRecipeImage:aPIRecipeImage andRecipeID:stringRecipeID andRecipeTitle:stringRecipeTitle andRecipeDetails:recipeSteps];
                                 
                                 [self.recipes addObject:self.recipe];
-                                
-                            }
+                            
                         }
                     }
                     [self.tableView reloadData];
